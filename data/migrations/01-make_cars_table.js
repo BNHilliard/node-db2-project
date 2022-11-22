c18188e8e3d6febc195f813f18b613e5a001efcf
timothy hoover <bnhilliard>
2022-11-21 17:29:03 -0800

    update

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,15 +1,15 @@
 exports.up = function (knex) {
   return knex.schema.createTable('cars', tbl => {
     tbl.increments();
-    tbl.string('vin').notNullable().unique();
-    tbl.string('make').notNullable();
-    tbl.string('model').notNullable();
-    tbl.decimal('mileage').notNullable();
-    tbl.string('title');
-    tbl.string('transmission');
+    tbl.string('vin', 17).notNullable().unique();
+    tbl.string('make', 120).notNullable();
+    tbl.string('model', 120).notNullable();
+    tbl.numeric('mileage').unsigned().notNullable();
+    tbl.string('title', 120);
+    tbl.string('transmission', 120);
   })
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists
+  return knex.schema.dropTableIfExists('cars')
 };
